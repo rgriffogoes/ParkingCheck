cad55cec746110ad2cee9f42eb8c8cd77126d8a2
Rafael Griffo Goes <rgriffogoes@gmail.com>
2012-07-25 17:53:34 -0300

    Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParkingCheck::Application.config.secret_token = 'df5464fe07ba72be09cc0f1a033928b1faf3d291b7d24a8787eb205967dc3fbe633d4f848bfc36e821788893f4dec1c6010aa90acedbb07345b0949290f8a2b1'
